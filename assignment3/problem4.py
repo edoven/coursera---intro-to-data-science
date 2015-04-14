b922261714c183c3167d45802c1910ef8bd1d1bc
godzy <edoardo.venturini@gmail.com>
2015-04-14 11:40:01 +0200

    problem 4 solved

---
+++ assignment3/problem4.py
@@ -0,0 +1,21 @@
+import MapReduce
+import sys
+
+"""
+Social graph "analyzer"
+"""
+
+mr = MapReduce.MapReduce()
+
+
+def mapper(record):   
+    mr.emit_intermediate(record[1][:-10], 1)
+
+
+def reducer(key, list_of_values): 
+    mr.emit((key))
+
+
+if __name__ == '__main__':
+	inputdata = open(sys.argv[1])
+  	mr.execute(inputdata, mapper, reducer)
